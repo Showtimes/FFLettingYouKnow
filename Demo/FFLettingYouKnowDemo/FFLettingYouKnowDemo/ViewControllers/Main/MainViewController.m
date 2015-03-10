3451c98717e424212d48b037c4a0e68667e0037d
Igor Matyushkin <igormatyushkin014@gmail.com>
2015-03-10 20:02:22 +0300

    Changes for demo project's MainViewController

--- Demo/FFLettingYouKnowDemo/FFLettingYouKnowDemo/ViewControllers/Main/MainViewController.m
+++ Demo/FFLettingYouKnowDemo/FFLettingYouKnowDemo/ViewControllers/Main/MainViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MainViewController.h"
+#import "FFLettingYouKnow.h"
 
 @interface MainViewController ()
 
@@ -14,14 +15,60 @@
 
 @implementation MainViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark Actions
+
+- (IBAction)pop1
+{
+    FFAlertClient *client = [FFAlertClient sharedAlertClientWithTitle:@"Test"
+                                                              message:@"This is a message talking about what we need to be doing."
+                                                    cancelButtonTitle:@"Ok"];
+    
+    FFAlertButton *secondButton = [[FFAlertButton alloc] initWithTitle:@"this is a second button title"
+    tapHandler:^
+    {
+    }];
+    
+    [client addButton:secondButton];
+    
+    [client showWithCompletion:^(BOOL isCancelled)
+    {
+    }];
+}
+
+- (IBAction)pop2
+{
+}
+
+- (IBAction)pop3
+{
+}
+
+- (IBAction)pop4
+{
+}
+
+- (IBAction)pop5
+{
+}
+
+- (IBAction)pop6
+{
+}
+
+- (IBAction)pop7
+{
 }
 
 @end
