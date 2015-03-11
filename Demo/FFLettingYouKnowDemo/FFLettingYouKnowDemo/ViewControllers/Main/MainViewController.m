4dcb3749465f2b7dc3bfc267d56c2c05dadc6728
James William Graham <wittedhaddock@gmail.com>
2015-03-11 15:28:35 -0400

    Basic alerting functionality showcased—last 3 buttons hidden for now

--- Demo/FFLettingYouKnowDemo/FFLettingYouKnowDemo/ViewControllers/Main/MainViewController.m
+++ Demo/FFLettingYouKnowDemo/FFLettingYouKnowDemo/ViewControllers/Main/MainViewController.m
@@ -31,32 +31,61 @@
 
 - (IBAction)pop1
 {
-    FFAlertClient *client = [FFAlertClient sharedAlertClientWithTitle:@"Test"
-                                                              message:@"This is a message talking about what we need to be doing."
+    FFAlertClient *client = [FFAlertClient sharedAlertClientWithTitle:@"Basic Alerting"
+                                                              message:@"This is a native alert with a cancel button"
                                                     cancelButtonTitle:@"Ok"];
     
-    FFAlertButton *secondButton = [[FFAlertButton alloc] initWithTitle:@"this is a second button title"
-    tapHandler:^
-    {
-    }];
-    
-    [client addButton:secondButton];
+
     
     [client showWithCompletion:^(BOOL isCancelled)
     {
+        if (isCancelled) {
+            NSLog(@"cancel received!");
+        }
     }];
 }
 
 - (IBAction)pop2
 {
+    
+    FFAlertClient *client = [FFAlertClient sharedAlertClientWithImplicitCancelAndTitle:@"Two button'ed alert" message:@"This is convience functionality for alerts with two buttons. The cancel button infers 'OK' as its copy" andSecondButtonTitle:@"A beautiful button" withSecondButtonHandler:^{
+        NSLog(@"second button engaged!");
+    }];
+    [client showWithCompletion:^(BOOL isCancelled) {
+        if (isCancelled) {
+            NSLog(@"cancelled like a champ");
+        }
+        //same block as second button handler
+    }];
 }
 
 - (IBAction)pop3
 {
+    FFAlertClient *client = [FFAlertClient sharedAlertClientWithMessage:@"Look Ma, No title! " cancelButtonTitle:@"Wow!"];
+    [client showWithCompletion:^(BOOL isCancelled) {
+        NSLog(@"biggest wow we've ever seen. nice.");
+    }];
 }
 
 - (IBAction)pop4
 {
+    FFAlertClient *client = [FFAlertClient sharedAlertClientWithTitle:@"A Witty Title" message:@"with an even wittier message.. what do you think?" cancelButtonTitle:@"Not witty at all"];
+    
+    FFAlertButton *newButton = [[FFAlertButton alloc] initWithTitle:@"The wittiest" tapHandler:^{
+        NSLog(@"thanks—we think so too");
+    }];
+    [client addButton:newButton];
+    
+    FFAlertButton *newerButton = [[FFAlertButton alloc] initWithTitle:@"Somewhat witty" tapHandler:^{
+        NSLog(@"pshht... whatever");
+    }];
+    [client addButton:newerButton];
+    
+    [client showWithCompletion:^(BOOL isCancelled) {
+        if (isCancelled) {
+            NSLog(@"what a terrible thing to say");
+        }
+    }];
 }
 
 - (IBAction)pop5
